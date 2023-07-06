5b3b1799697b3a2daa6a91a1d0eece52503e9101
Jackkka520 <zhangc99.22@ichat.sp.edu.sg>
2023-07-06 09:51:59 +0800

    test

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const connection = require('./db'); //Import from db.js
 
 
+const path = require('path');
 
 //////////////////////////////////////////////////////
 // INIT
@@ -13,14 +14,15 @@ const connection = require('./db'); //Import from db.js
 const app = express();
 console.log("process.env.PORT:", process.env.PORT);
 const PORT = process.env.PORT || 3000;
-
 //////////////////////////////////////////////////////
 // SETUP APP
 //////////////////////////////////////////////////////
 app.use(cors());
 app.use(express.json());
 
-app.use("/", express.static("../frontend"));
+app.use("/legacy", express.static("../frontend"));
+app.use("/", express.static("../frontend-react/build"));
+app.use(express.static(path.join(__dirname, '../frontend-react/build')));
 
 //////////////////////////////////////////////////////
 // DISPLAY SERVER RUNNING
@@ -28,18 +30,23 @@ app.use("/", express.static("../frontend"));
 app.get('/',(req,res)=> {
     res.send(`Server running on port ${PORT}`)
 });
-
 app.listen(PORT,()=> {
     console.log(`App listening to port ${PORT}`);
 });
-
 //////////////////////////////////////////////////////
 // SETTING MAIN ROUTES
 //////////////////////////////////////////////////////
 const mainRoutes = require("./routes/mainRoutes");
 app.use("/api", mainRoutes);
 
-
 app.get("/message", (req, res, next) => {
     res.send(req.query);
 });
+
+
+//////////////////////////////////////////////////////
+// FOR SINGLE PAGE APPLICATION
+//////////////////////////////////////////////////////
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend-react/build', 'index.html'));
+  });
